ff8a8c1a745f22660d9a35381da08d6161026391
ʄɽɑɳƙꍟɳʂʈꍟꀤɳ™ ❬❬kiռց ૨ip⛦❭❭ <64843897+DONOFTELEGRAM@users.noreply.github.com>
2021-04-21 23:51:40 +0530

    Update Logo_Maker.py

--- Harita/modules/Logo_Maker.py
+++ Harita/modules/Logo_Maker.py
@@ -43,7 +43,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @HaritaSupport, {e}')
+   await event.reply(f'Error Report @AndreaSupportChat, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
